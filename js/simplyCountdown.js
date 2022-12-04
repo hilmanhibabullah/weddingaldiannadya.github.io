763ee52c6fed5c2f6e0951f3ef7b6a3bf7c78cf2
hilmanhibabullah <hilman8585@gmail.com>
2022-12-05 00:26:33 +0700

    2

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -294,7 +294,7 @@ var x = setInterval(function(){
     var minutes = Math.floor((distance % (1000*60*60)) / (1000*60) );
     var seconds = Math.floor((distance % (1000*60))/1000);
 
-    document.getElementById("demo").innerHTML = days +" days " +hours+ " hours " +minutes+ " minutes " +seconds+ " seconds ";
+    document.getElementById("demo").innerHTML = days +" days " +hours+ " hours " +minutes+ " minutes " +seconds+ " seconds to main ceremony";
 
     if(distance < 0){
         clearInterval(x);
